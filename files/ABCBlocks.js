654fd92b5032d5d70f475d95be804bfdc74db3f0
Q8aARt <Q8aARt@cyber-dojo.org>
2023-02-07 12:36:37 +0000

    7 ran tests, predicted green, got red

--- files/ABCBlocks.js
+++ files/ABCBlocks.js
@@ -1,8 +1,37 @@
 'use strict';
 
+const blocks = [
+    ['B','O'],
+    ['X','K'],
+    ['D','Q'],
+    ['C','P'],
+    ['N','A'],
+    ['G','T'],
+    ['R','E'],
+    ['T','G'],
+    ['Q','D'],
+    ['F','S'],
+    ['J','W'],
+    ['H','U'],
+    ['V','I'],
+    ['A','N'],
+    ['O','B'],
+    ['E','R'],
+    ['F','S'],
+    ['L','Y'],
+    ['P','C'],
+    ['Z','M']
+]
+
+const any = (array, fn = Boolean) => array.some(fn);
 function can_make_word(word) {
-  let returnValue = false;
-  return returnValue;
+    let returnValue = false;
+    if (!!word) {
+        returnValue = word.reduce((acc, value) => {
+            return acc && any(blocks, (block) => block.contains(value))
+        }, returnValue);
+    }
+    return returnValue;
 }
 
 module.exports = can_make_word;
