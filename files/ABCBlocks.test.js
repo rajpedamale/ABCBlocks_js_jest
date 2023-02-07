87600b82edb303fe9d71890f5a7d679466044db1
Q8aARt <Q8aARt@cyber-dojo.org>
2023-02-07 12:13:47 +0000

    4 ran tests, predicted red, got amber

--- files/ABCBlocks.test.js
+++ files/ABCBlocks.test.js
@@ -6,4 +6,9 @@ describe('ABC Blocks', () => {
   it('should return a boolean', () => {
     expect(typeof can_make_word('')).toEqual('boolean');
   });
+  
+  it('should return true for "a"', () => {
+    expect(can_make_word('a')).toBeTruthy());
+  });
+  
 });
